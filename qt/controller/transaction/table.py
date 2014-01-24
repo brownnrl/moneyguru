fe7339550be937635636f53f5a0c31e7b159f38c
brownnrl <brownnrl@gmail.com>
2014-01-24 07:12:23 -0500

    (#14 #15) Auto-Resize and Nicely Aligned Currencies
    Second stab at creating the delegate to paint the currency information
    aligned.  Also tried to fix the column widths for the amount field
    and adjust them on the fly.

--- qt/controller/transaction/table.py
+++ qt/controller/transaction/table.py
@@ -6,23 +6,53 @@
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/bsd_license
 
-from PyQt4.QtCore import Qt
+from PyQt4.QtCore import Qt, QRectF, QSize
 from PyQt4.QtGui import QPixmap
+from PyQt4.QtGui import QStyle, QFontMetrics, QTextOption, QHeaderView, QStyleOptionViewItemV4
+import os, string
 
 from qtlib.column import Column
 from ...support.item_delegate import ItemDecoration
 from ..table import TableDelegate, DATE_EDIT, DESCRIPTION_EDIT, PAYEE_EDIT, ACCOUNT_EDIT
 from ..table_with_transactions import TableWithTransactions
 
+from core.gui.transaction_table import TotalRow
+
+try:
+    if os.environ.get('USE_PY_AMOUNT'):
+        raise ImportError()
+    from core.model._amount import Amount
+except ImportError:
+    print("Using amount_ref")
+    from core.model._amount_ref import Amount
+
+class Del:
+    def __init__(self, keep):
+        self.keep = dict((ord(c), c) for c in keep)
+
+    def __getitem__(self, k):
+        if k in self.keep:
+            return k
+        else:
+            return None
+
 class TransactionTableDelegate(TableDelegate):
-    def __init__(self, model):
+    def __init__(self, model, view):
         TableDelegate.__init__(self, model)
         arrow = QPixmap(':/right_arrow_gray_12')
         arrowSelected = QPixmap(':/right_arrow_white_12')
+        self._view = view
+        amount_index = self._model.columns.column_by_name('amount').ordered_index
+        print(amount_index)
+        self._view.horizontalHeader().setResizeMode(amount_index, QHeaderView.ResizeToContents)
         self._decoFromArrow = ItemDecoration(arrow, self._model.show_from_account)
         self._decoFromArrowSelected = ItemDecoration(arrowSelected, self._model.show_from_account)
         self._decoToArrow = ItemDecoration(arrow, self._model.show_to_account)
         self._decoToArrowSelected = ItemDecoration(arrowSelected, self._model.show_to_account)
+
+        self._valid_chars = list(string.digits)
+        self._valid_chars.extend([',', '.'])
+        self._keep_chars = Del(self._valid_chars)
     
     def _get_decorations(self, index, isSelected):
         column = self._model.columns.column_by_index(index.column())
@@ -32,6 +62,102 @@ class TransactionTableDelegate(TableDelegate):
             return [self._decoToArrowSelected if isSelected else self._decoToArrow]
         else:
             return []
+
+    def _get_amount_from_index(self, index):
+        if not index.isValid():
+            return None
+
+        column = self._model.columns.column_by_index(index.column())
+
+        if column.name != 'amount':
+            return None
+
+        row = self._model[index.row()]
+
+        if not hasattr(row, 'transaction'):
+            return None  # No information to work with
+
+        if hasattr(row.transaction.amount, 'value'):
+            amount = row.transaction.amount
+        else:
+            amount = Amount(row.transaction.amount, self._model.document.default_currency)
+
+        return amount
+
+    def _get_amount_texts(self, amount, option):
+        do_paint_currency = amount.currency != self._model.document.default_currency
+        val_string = self._model.document.format_amount(amount).translate(self._keep_chars)
+        cur_code = amount.currency.code
+        cur_width = option.fontMetrics.width(amount.currency.code) if do_paint_currency else 0
+        val_width = option.fontMetrics.width(val_string)
+
+        return cur_width, val_width, cur_code, val_string
+
+
+    def sizeHint(self, option, index):
+
+        amount = self._get_amount_from_index(index)
+
+        if amount is None:
+            return TableDelegate.sizeHint(self, option, index)
+
+        option = QStyleOptionViewItemV4(option)
+
+        cur_width, val_width, _, _ = self._get_amount_texts(amount, option)
+
+        return QSize(cur_width+val_width+5, option.fontMetrics.height())
+
+
+    def _paint_currency(self, painter, option, amount, is_selected):
+
+        option = QStyleOptionViewItemV4(option)
+
+        painter.setFont(self._view.font())
+
+        cur_width, val_width, cur_code, val_string = self._get_amount_texts(amount, option)
+
+        font_height = option.fontMetrics.height()
+
+        do_paint_currency = cur_width > 0
+
+        if is_selected:
+            painter.setPen(Qt.white)
+        else:
+            painter.setPen(Qt.black)
+
+        if do_paint_currency:
+            painter.drawText(QRectF(option.rect.left(),
+                                    option.rect.top(),
+                                    cur_width,
+                                    font_height),
+                             cur_code,
+                             QTextOption(Qt.AlignVCenter))
+
+        painter.drawText(QRectF(option.rect.right() - val_width,
+                                option.rect.top(),
+                                val_width,
+                                font_height),
+                         val_string,
+                         QTextOption(Qt.AlignVCenter))
+
+
+
+    def paint(self, painter, option, index):
+        # Paint cells as we normally would
+        TableDelegate.paint(self, painter, option, index)
+
+        is_selected = bool(option.state & QStyle.State_Selected)
+
+        amount = self._get_amount_from_index(index)
+
+        if amount is None:
+            return
+
+        self._paint_currency(painter,
+                             option,
+                             amount,
+                             is_selected)
+
     
 
 class TransactionTable(TableWithTransactions):
@@ -49,8 +175,14 @@ class TransactionTable(TableWithTransactions):
     
     def __init__(self, model, view):
         TableWithTransactions.__init__(self, model, view)
-        self.tableDelegate = TransactionTableDelegate(self.model)
+        self.tableDelegate = TransactionTableDelegate(self.model, self.view)
         self.view.setItemDelegate(self.tableDelegate)
         self.view.sortByColumn(1, Qt.AscendingOrder) # sorted by date by default
         self.view.deletePressed.connect(self.model.delete)
-    
+
+    def _getData(self, row, column, role):
+        if not isinstance(row, TotalRow) and role == Qt.DisplayRole and column.name == 'amount':
+            return ""  # handled by the paint method in the delegate.
+        else:
+
+            return TableWithTransactions._getData(self, row, column, role)
