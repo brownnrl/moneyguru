37f6643b6992f14f6630ece2c7889157807e1432
brownnrl <brownnrl@gmail.com>
2014-02-01 21:52:40 -0500

    (#14 #15) Amount Painting
    Update copyright information on files touched during this feature
    implementation.

--- qt/support/column_view.py
+++ qt/support/column_view.py
@@ -1,6 +1,13 @@
-__author__ = 'nelson'
+# -*- coding: utf-8 -*-
+# Created By: Nelson Brown
+# Created On: 2014-02-01
+# Copyright 2014 Hardcoded Software (http://www.hardcoded.net)
+#
+# This software is licensed under the "BSD" License as described in the "LICENSE" file,
+# which should be included with this package. The terms are also available at
+# http://www.hardcoded.net/licenses/bsd_license
 
-from .item_delegate import ColumnDelegate
+__author__ = 'nelson'
 
 from collections import namedtuple
 
